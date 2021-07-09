41a638685f7a93c14cfe8bb15667806f8f136640
fpcodegit <fpcodegit@gmail.com>
2021-07-09 10:29:30 -0400

    Configured the config server

---
+++ src/main/java/cl/fp/config/server/pokedex/configuration/ConfigServerConfiguration.java
@@ -0,0 +1,9 @@
+package cl.fp.config.server.pokedex.configuration;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfiguration {
+}
