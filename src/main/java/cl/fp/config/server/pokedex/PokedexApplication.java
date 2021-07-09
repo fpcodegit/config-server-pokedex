6654487edf7514be24595b1f8cd4f09b7c1f1d48
fpcodegit <fpcodegit@gmail.com>
2021-07-09 10:23:29 -0400

    First commit

---
+++ src/main/java/cl/fp/config/server/pokedex/PokedexApplication.java
@@ -0,0 +1,13 @@
+package cl.fp.config.server.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexApplication.class, args);
+    }
+
+}
